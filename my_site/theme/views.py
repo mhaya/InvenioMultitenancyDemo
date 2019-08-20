a00794ff8b9db527329bbc793d27bc640af8d967
mhaya <masaharu.hayashi3@gmail.com>
2019-08-20 22:15:31 +0900

    Fix view

--- my_site/theme/views.py
+++ my_site/theme/views.py
@@ -16,9 +16,33 @@ from __future__ import absolute_import, print_function
 
 from flask import Blueprint
 
+import os
+from invenio_app.factory import instance_path, static_folder
+from flask import current_app as flask_current_app
+
+
 blueprint = Blueprint(
     'my_site',
     __name__,
     template_folder='templates',
     static_folder='static',
 )
+
+
+@blueprint.route('/env')
+def hello():
+    L = []
+    L.append("<h1>os.environ.items()</h1>")
+    for k, v in os.environ.items():
+        L.append("{key} : {value}".format(key=k, value=v))
+
+    
+    L.append("<h1>invenio_app()</h1>")
+    L.append("instance_path:"+instance_path)
+    L.append("static_folder:"+static_folder)
+
+    L.append("<h1>flask_current_app</h1>")
+    for k, v in flask_current_app.config.items():
+        L.append("{key} : {value}".format(key=k, value=v))
+
+    return "</br>".join(L)
